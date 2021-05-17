21514353a76af9afd68eafd6f50d565d15f7ad11
Clare <chblackburne@gmail.com>
2021-05-17 19:40:17 +0100

    Basic setup of server and example route

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('/public/'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/example', function (req, res) {
+  res.send('template');
+});
+
+app.use(function (req, res, next) {
+  res.status(404).render('404', { missingUrl: req.originalUrl });
 });
 
 app.listen(port, () => {
