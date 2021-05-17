329e4ef85c5347a4877e69767378c54dcc42772f
Clare <chblackburne@gmail.com>
2021-05-17 22:23:25 +0100

    Use turndown to convert html back to markdown

--- __tests__/content.test.js
+++ __tests__/content.test.js
@@ -1,13 +1,21 @@
 const request = require('supertest');
+const { readFileSync } = require('fs');
+
 const app = require('../app');
 const { recursiveRoutes } = require('../services/routegenerator');
+const TurndownService = require('turndown');
+const turndownService = new TurndownService();
 
 describe('Page content', () => {
   it('should return html for routes', async (done) => {
     const routes = recursiveRoutes('./content');
-    for (const route of routes) {
-      const res = await request(app).get(route);
+    for (let route = 0; route < routes.length; route++) {
+      const res = await request(app).get(routes[route]);
       expect(res.type).toEqual('text/html');
+      const markdown = readFileSync('.' + routes[route] + '/index.md', 'utf8');
+      expect(turndownService.turndown(res.text)).toContain(
+        markdown.split('\n')[1]
+      );
     }
     done();
   });
